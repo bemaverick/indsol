c2e9064c15d5a0b7b4c7d2a258e243531b8a84d1
Serhii Morykon <sergomen153@gmail.com>
2017-03-14 01:03:18 +0200

    work on footer

---
+++ js/script.js
@@ -0,0 +1,24 @@
+ function initialize() {
+	    var x = 50.449763;
+	    var y = 30.424601;
+	    var mapOptions = {
+	        zoom: 15,
+	        center: new google.maps.LatLng(x, y),
+	        scrollwheel: true,
+	        disableDefaultUI: false
+	    }
+	    var map = new  google.maps.Map(
+	        document.querySelector(".footer-map"),
+	        mapOptions
+	    );
+	    var image = "img/gmap.png"; 
+	    var myLatLng = new google.maps.LatLng(x, y);
+	    var beachMarker = new google.maps.Marker({
+	        position: myLatLng,
+	        map: map,
+	        icon: image
+	    });
+}
+google.maps.event.addDomListener(window, "load", initialize);
+
+
